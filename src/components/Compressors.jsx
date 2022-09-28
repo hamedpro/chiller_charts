36ba4006d94e1e74fd11fed1a275d018ad8b63cf
hamedpro <hamedyaghootpour@gmail.com>
2022-09-29 00:39:27 +0330

    backup'

--- src/components/Compressors.jsx
+++ src/components/Compressors.jsx
@@ -1,26 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { custom_ajax } from "../custom_ajax";
 
-function Compressor({compressor_index}) {
+function ChartView({type,compressor_index=undefined}) {
 	var [data, set_data] = useState(null)
 	var [filters, set_filters] = useState({
 	})
 	var [charts,set_charts] = useState([])
 	function build_chart_data() {
-		var field_names = Object.keys(data.logs[0].compressors[0]);
-		var chart_data = {
-			labels: data.logs.map(log=>new Date(log.date).toUTCString()),
-			datasets: Object.keys(data.logs[0].compressors[compressor_index].data).map(compressor_data_field_name => {
-				return ({
-						label: compressor_data_field_name,
-					data: data.logs.map(log => {
-							return log.compressors[compressor_index].data[compressor_data_field_name]
+		var chart_data;
+		if (type === "common") {
+			chart_data = {
+				labels: data.logs.map(log => new Date(log.date).toUTCString()),
+				datasets: Object.keys(data.logs[0].common).map(common_data_field_name => {
+					return {
+						label: common_data_field_name,
+						data: data.logs.map(log => {
+							return log.common.data[common_data_field_name]
+						}),
+						backgroundColor: "rgba(255, 99, 132, 0.2)",
+						borderColor: "rgba(66, 40, 181, 0.8)",
+						borderWidth: 1,
+					}
+				})
+			}
+		} else if (type === "compressor") {
+			var field_names = Object.keys(data.logs[0].compressors[0]);
+			chart_data= {
+				labels: data.logs.map(log=>new Date(log.date).toUTCString()),
+				datasets: Object.keys(data.logs[0].compressors[compressor_index].data).map(compressor_data_field_name => {
+					return ({
+							label: compressor_data_field_name,
+						data: data.logs.map(log => {
+								return log.compressors[compressor_index].data[compressor_data_field_name]
 						}),
 						backgroundColor: "rgba(255, 99, 132, 0.2)",
 						borderColor: "rgba(66, 40, 181, 0.8)",
 						borderWidth: 1,
 					})
-			})
+				}),
+			}
 		}
 		console.log(chart_data)
 		//filtering the data depending on "filters" state
@@ -33,7 +51,7 @@ function Compressor({compressor_index}) {
 			var ctx = document.getElementById("common_chart"+compressor_index).getContext("2d");
 			charts[0] = new Chart(ctx, {
 				type: "line",
-				data :build_chart_data(0),
+				data: build_chart_data(),
 				options: {
 					scales: {
 						y: {
@@ -44,7 +62,7 @@ function Compressor({compressor_index}) {
 			});
 		} else {
 			//todo just update changed parts
-			charts[0].data = build_chart_data(0)
+			charts[0].data = build_chart_data(),
 			charts[0].update()
 		}
 		
@@ -97,10 +115,12 @@ export function Compressors() {
 			{data === null && (
 				<h1>loading the data ...</h1>
 			)}
-			{data !== null && data.logs[0].compressors.map((compressor,index) => {
+			{data !== null && (
+				<ChartView type="common"/>
+			) && data.logs[0].compressors.map((compressor, index) => {
 				return (
 					<React.Fragment key={index}>
-						<Compressor compressor_index={index}/>
+						<ChartView type="compressor" compressor_index={index}/>
 					</React.Fragment>
 					
 				)
